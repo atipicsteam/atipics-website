a10020cc1f15d25517a8785200f1dbc576c9d1c1
azfyxi <azfyxi@protonmail.com>
2026-07-07 12:51:52 +0200

    chore: update @astrojs/vercel import to support broader deployment targets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
     output: 'server',
