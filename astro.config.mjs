2b9d06e28b49105e84a96de3582a463b76b83cb8
azfyxi <azfyxi@protonmail.com>
2026-07-08 10:49:44 +0200

    fix: revert outDir to dist and force Vercel to include dist via vercelignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-    outDir: './astro-dist',
     output: 'static',
     adapter: vercel(),
     integrations: [react(), markdoc(), keystatic()],
